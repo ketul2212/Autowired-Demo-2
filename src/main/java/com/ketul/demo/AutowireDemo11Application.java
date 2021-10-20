089a5592da15da2ae78e67afcce5f3a1e693b68b
ketul2212 <pketul2212@gmail.com>
2021-10-20 16:54:34 +0530

    SS

---
+++ src/main/java/com/ketul/demo/AutowireDemo11Application.java
@@ -0,0 +1,13 @@
+package com.ketul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowireDemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowireDemo11Application.class, args);
+	}
+
+}
